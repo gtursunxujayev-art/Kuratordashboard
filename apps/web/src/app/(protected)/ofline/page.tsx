2f692073128b987e4756e5bfa332ca52e55a74d9
najotnurtheg <gtursunxujayev@gmail.com>
2026-04-16 23:21:05 +0500

    Update page.tsx

--- apps/web/src/app/(protected)/ofline/page.tsx
+++ apps/web/src/app/(protected)/ofline/page.tsx
@@ -1,6 +1,5 @@
-import { redirect } from 'next/navigation';
+import DashboardPage from '../dashboard/page';
 
 export default function OflinePage() {
-  redirect('/dashboard?category=offline');
+  return <DashboardPage forcedCategory="offline" />;
 }
-
