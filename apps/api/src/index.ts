966030599d85beb4621a6d040d4c4b4372ee08b3
najotnurtheg <gtursunxujayev@gmail.com>
2026-04-19 12:04:38 +0500

    Update index.ts

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,9 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app';
+import { assertJWTConfiguration } from './services/auth/jwt';
 
 const PORT = process.env.PORT || 3001;
 const HOST = '0.0.0.0';
+
+assertJWTConfiguration();
 
 app.listen(Number(PORT), HOST, () => {
   console.log(`Kuratordashboard API running on ${HOST}:${PORT}`);
