ad4f8a0bdc43c8cb8ebf348dea9b1e5536aa2d1c
gtursunxujayev-art <gtursunxujayev@gmail.com>
2026-09-15 15:07:04 +0500

    Update index.ts

--- apps/api/src/trpc/routers/index.ts
+++ apps/api/src/trpc/routers/index.ts
@@ -7,6 +7,8 @@ import { settingsRouter } from './settings';
 import { kuratorsRouter } from './kurators';
 import { faceidRouter } from './faceid';
 import { intensivRouter } from './intensiv';
+import { clientBotRouter } from './client-bot';
+import { messagesRouter } from './messages';
 
 export const appRouter = router({
   auth: authRouter,
@@ -17,6 +19,8 @@ export const appRouter = router({
   kurators: kuratorsRouter,
   faceid: faceidRouter,
   intensiv: intensivRouter,
+  clientBot: clientBotRouter,
+  messages: messagesRouter,
 });
 
 export type AppRouter = typeof appRouter;
